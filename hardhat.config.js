159ecda5e71b1fbd739d0272027bfefc6ff6c8d9
0x-ak <oxak_punk@pm.me>
2022-08-03 00:09:08 +1000

    Initialise

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+//require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+//require("@nomicfoundation/hardhat-chai-matchers");
+require("dotenv").config()
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: [GOERLI_URL] ,
+      accounts: [PRIVATE_KEY]
+    }
+  }
+  
+};
+
+
